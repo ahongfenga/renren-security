67f532f2492b029593c4da68f4ecb100861c45bd
ahongfenga <1004439185@qq.com>
2018-04-18 22:50:36 +0800

    git使用测试

--- renren-admin/src/main/java/io/renren/AdminApplication.java
+++ renren-admin/src/main/java/io/renren/AdminApplication.java
@@ -6,15 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-
+/*
+ * test
+ */
 @SpringBootApplication
 @MapperScan(basePackages = {"io.renren.modules.*.dao"})
 public class AdminApplication extends SpringBootServletInitializer {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
 	}
 
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(AdminApplication.class);
